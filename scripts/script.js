d3b17f5575773cf19370fa53b54b6d7bd3ba1d4c
jgsack <jgsack@gmail.com>
2017-03-11 17:02:00 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,9 @@
     .controller('HomeController', ['$scope', function ($scope) {
       $scope.header = "Life as a Realizer";
     }])
-    .controller('ReviewsController', ['$scope', function ($scope) {      
+    .controller('SchoolController', ['$scope', function ($scope) {      
+    }])  
+    .controller('WithController', ['$scope', function ($scope) {      
     }])    
     .controller('NavController', ['$scope', '$location', function ($scope, $location) { 
        $scope.isActive = function (viewLocation) { 
@@ -55,9 +57,9 @@
                 templateUrl: 'partials/home.html',
                 controller: 'HomeController'
               }).
-              when('/reviews', {
-                templateUrl: 'partials/reviews.html',
-                controller: 'ReviewsController'
+              when('/school', {
+                templateUrl: 'partials/school.html',
+                controller: 'SchoolController'
               }).
               when('/book', {
                 templateUrl: 'partials/freebook.html',
@@ -85,7 +87,7 @@
               }).              
               when('/with', {
                 templateUrl: 'partials/with.html',
-                controller: 'ReviewsController'
+                controller: 'WithController'
               }).
               when('/links', {
                 templateUrl: 'partials/links.html',
@@ -105,19 +107,6 @@
        }
        return slides
     }    
-// var innocence = angular.module('innocence', ['ngRoute'])
-// .config(['$routeProvider',
-//     function($routeProvider) {
-//         $routeProvider.
-//           when('/home', {
-//             templateUrl: 'partials/home.html',
-//             controller: 'HomeCtrl'
-//           }).
-//           otherwise({ redirectTo: '/home' });
-//     }]);
-// innocence.controller('HomeCtrl', ['$scope', function ($scope) {
-//   $scope.header = "Have you ever read such a story?";
-// }]);
 
 
 
@@ -138,90 +127,3 @@
 
 
 
-
-
-
-
-
-
-
-/*
-
-
-
-//**************************************************OLD
-
-document.write('<style type="text/css">body{display:none}</style>');
-(function ($) {
-    // VERTICALLY ALIGN FUNCTION
-    $.fn.vAlign = function () {
-        return this.each(function (i) {
-            var ah = $(this).height();
-            var wh = $(window).height();
-            if (wh > 620) {
-                var mh = (wh - ah) / 2;
-                $(this).css('margin-top', mh);
-            }
-        });
-    };
-})(jQuery);
-
-$(document).ready(function () {
-    $('#art').hide();
-    $('body').css('display', 'block'); //avoiding flickering
-    $(".accordion").accordion({
-        //  active: false,
-        fillSpace: true
-    });
-
-    $("a.preview").click(function () {
-        $('div.myImage').dialog({ width: 810,
-        height: 570 });
-    });
-
-    var tabContainers = $('div.tabs > div');
-    $('div.tabs ul.tabNavigation a').click(function () {
-        tabContainers.hide().filter(this.hash)
-                .show();
-        //.fadeIn(300);
-
-        $('div.tabs ul.tabNavigation a').removeClass('selected');
-        $(this).addClass('selected');
-
-        return false;
-    }).filter(':first').click();
-
-    //vertically align page at start and when window is resized
-    $("#page").vAlign();
-    $(window).resize(function () {
-        $("#page").vAlign();
-    });
-
-    var loaded = false;
-    $('#galleryLink').click(function () {
-        if (!loaded) {
-            $('#galleryframe').attr('src', 'gallery.htm');
-            loaded = true;
-        }
-    });
-    $('#artLink').click(function () {
-        //$('#art').html('<img id="gohome" src="Art/Home.png" /><iframe id="artframe" src=art.htm height="800px" width="100%" frameborder="0"></iframe>');
-        //$('#artframe').attr('src', 'art.htm');
-        $('#page').hide();
-        $('#art').show();
-        // $('#page').css('width', '100%');
-        // $('.tabs > div ').css('background-image', 'none');
-    });
-
-    $('#gohome').click(function () {        //show page and get rid of art            
-        $('#art').hide();
-        $('#page').show();
-        $('#homea').trigger('click');
-    });
-
-    $('#paperback').click(function () {
-        $('#getbook').click();
-    });
-
-});   
-    */
